02b66c08194346ef253978497eb3c69986b0ad0c
Abhayjeet Kumar <112002642+21-abhay@users.noreply.github.com>
2024-04-15 15:51:10 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 let mongoURI = process.env.mongoURI || "mongodb+srv://abhay:royabhayjeet@abhay.7je5cm5.mongodb.net/massgyan"
 
 const connectToMongo = ()=>{
-    mongoose.connect(mongoURI);
+    mongoose.connect(process.env.mongoURI);
     console.log("Connected")
 }
 
